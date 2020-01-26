845fb40047e7981a36f64d5a546d37cb780f0cff
Víctor <vlopez@quadram.mobi>
2020-01-26 11:04:58 +0100

    Set PHPUnit settings and write simple test

---
+++ src/Providers/LaravelPassportServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Quadram\LaravelPassport\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPassportServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Quadram\LaravelPassport\Test\Unit;
+
+use Quadram\LaravelPassport\Test\TestCase;
+
+class BasicTest extends TestCase
+{
+
+    /** @test */
+    public function basic_test()
+    {
+        $this->assertTrue(true);
+    }
+
+}
